37c75e6b23e0a6c6bb88ae62eeb709e75ce5f0cc
clarkgh <demolisho@hotmail.com>
2017-08-13 14:06:57 -0700

    Lesson1 draft2

--- Scope/lesson1.js
+++ Scope/lesson1.js
@@ -1,35 +1,41 @@
-// Playing with scope
+// Playing with scope (the context of objects)
 
-// copy and paste 'this' into your web console
+// Example 1:
+// copy and paste line 4 into your web console
 console.log(this);
-// run
 
-// You will see the context for your browser's Window object
+// When ran, you will see the context for your browser's Window object
 
-this // this returns the context of the global object when it is inside global "Scope"
+// 'this' returns the context of the global object when it is inside global "Scope"
 // scope is the context of the object the method belongs to
 
-// copy and paste the below 
+
+//Example 2:
+
+// copy and paste lines: 14-21
 var kitten = {
   name: "Mr. sparkles", //set a property
   sayName: function() { //create a method
     console.log( this );
-    // console.log( this.name );
   }
 }
 
 kitten.sayName(); //here we call the sayName method within the kitten
 //you'll now see the context for the kitten object returned
 
-//now refresh, then copy and paste this into the browser
+//Example 3:
+
+//refresh or new browser, then copy and paste lines: 25-32
 var kitten = {
   name: "Mr. sparkles",
   sayName: function() {
-    // console.log(this);
     console.log( "Meow, my name is, " + this.name );
   }
 }
 
-kitten.sayName(); //Why do you think this is returning the name value?
+console.log( "Meow, my name is, " + kitten.name );
+kitten.sayName();
 
+//note that the two are logging the same value.
 
+//for now, think of 'this', as a way to have access to the context of your object
