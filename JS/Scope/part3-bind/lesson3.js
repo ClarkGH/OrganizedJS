0d810506405e744efc732459caad9ede13d86d9a
clarkgh <demolisho@hotmail.com>
2017-08-18 10:50:05 -0700

    bind explained

---
+++ JS/Scope/part3-bind/lesson3.js
@@ -0,0 +1,27 @@
+// WTF is bind doing?
+
+// Every function object has 3 internal methods, apply(), bind(), and call()
+// .bind() - takes an object as an argument, will attach the argument's scope to the function it runs on and will be bound to it
+
+//Example 3
+//no need to run, conceptually the same as example2, only with explanations
+
+var animal = {
+  species: 'dog',
+  says: 'woof',
+  getSaying: function() {
+    return ('The ' + this.species + ' says ' + this.says);
+  }
+}
+
+var logSaying = function() {
+  console.log( this.getSaying() );
+}//here we only have access to the global context
+// how do we break out of it?
+// we need to give it the context of the object we want
+
+var sayTheThing = logSaying.bind(animal); //here I set a variable to the function with the animal object's context bound to it
+
+sayTheThing(); //here I run the function
+
+// the 'this', on line 18, is pointing to animal's scope and now has access to its properties, methods, and values
