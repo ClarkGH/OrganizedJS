b8970f9cb20f3774c80cc363eaf1d7b0175f8877
clarkgh <demolisho@hotmail.com>
2017-08-21 15:37:16 -0700

    currying with bind()

--- JS/Scope/part3-bind/lesson5.js
+++ JS/Scope/part3-bind/lesson5.js
@@ -1,1 +1,16 @@
-// Currying
+// Currying with bind
+
+// example 5
+// copy and paste into console
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+var plusTwo = sum.bind(this, 2); // notice the binding of scope
+
+console.log(plusTwo(2));
+console.log(plusTwo(4));
+
+// what do you see being printed?
+// why do you think that is?
