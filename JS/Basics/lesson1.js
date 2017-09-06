d2311daab1bf6cf2824c8316b970686e23e53dc6
clarkgh <demolisho@hotmail.com>
2017-09-06 13:18:26 -0700

    primitives objects overview

---
+++ JS/Basics/lesson1.js
@@ -0,0 +1,30 @@
+/* What Forms Javascript
+
+Javascript has 7 different data types
+  1. Numbers
+  2. Strings
+  3. Booleans
+  4. Symbol
+  5. Undefined
+  6. Null
+  7. Objects
+
+Each data type that is NOT AN OBJECT in JS, is a primitive value or primitive data type.
+
+Everything else is an object 
+*/
+
+//Examples
+
+1; // number
+"hi"; //string
+true; // false
+Symbol('foo'); //symbol
+undefined; //undefined
+null; //null
+function () {} //object
+[1, 2, 3] //object 'holding' primitive values
+//etc.
+
+// I recommend learning primitives first.
+// Then dive into objects.
