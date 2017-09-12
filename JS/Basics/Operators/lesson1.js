110e9e54b3c965da1e6c38b806958ac1c168f387
clarkgh <demolisho@hotmail.com>
2017-09-11 17:05:31 -0700

    operands and operators

---
+++ JS/Basics/Operators/lesson1.js
@@ -0,0 +1,14 @@
+// Operators and Operands
+
+/*
+  In javascript, operators are special functions that work in their own ways
+  Think of operators as symbols that do specific tasks (special functions)
+  Think of operands as the things operands do things with (parameters)
+*/
+
+// Example 1: basic operator and operands
+1 + 2; // in this case, 1 and 2 are operands. The '+' symbol is the operator
+
+// Try to think of a few operators you've seen in javascript
+// What do they do?
+// How are they different from normal functions?
