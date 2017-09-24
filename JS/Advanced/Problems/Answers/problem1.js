96cc517361b3c2ed79b677f8787874e7479d6b6e
clarkgh <demolisho@hotmail.com>
2017-09-23 21:05:12 -0700

    putting problems in here now

---
+++ JS/Advanced/Problems/Answers/problem1.js
@@ -0,0 +1,11 @@
+// Find and print out a list of unique triplets 
+// that sum up to the specified value
+
+function findTriplets( array, value ) {
+  var array = array.sort(),
+    length = array.length;
+
+  for ( var i = 0; i < length; i++ ) {
+    
+  }
+}
