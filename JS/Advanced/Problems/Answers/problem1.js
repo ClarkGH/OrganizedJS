771c96f8fd6a98b7c6cb89098a97ed8f5bec3172
clarkgh <demolisho@hotmail.com>
2017-09-24 17:52:37 -0700

    extending problem 1 to problem 2

--- JS/Advanced/Problems/Answers/problem1.js
+++ JS/Advanced/Problems/Answers/problem1.js
@@ -6,6 +6,19 @@ function findTriplets( array, value ) {
     length = array.length;
 
   for ( var i = 0; i < length; i++ ) {
-    
+    var j = i + 1,
+      k = length - 1;
+    while ( j < k ) {
+      if (arr[i] + arr[j] + arr[k] < value) {
+        j++;
+      } else if (arr[i] + arr[j] + arr[k] > value) {
+        k--;
+      } else {
+        console.log(arr[i] + "," + arr[j] + "," + arr[k]);
+        j++;
+        k--;
+      }
+    }
   }
+  return true;
 }
---
+++ JS/Advanced/Problems/Answers/problem2.js
@@ -0,0 +1,16 @@
+// You're given an array of numbers
+// You need to find the highest possible product you can find with three numbers in the array
+
+/* More explanation for the confused */
+// imagine that you have the following array
+const exampleArray = [18, 97, 2, 9, 3, 0, 26, 0, 26, 15, 5, 18, 43];
+
+// from looking at it at first: 
+// Question? Can you tell which numbers will create the greatest number when multiplied against one another?
+// Answer: It's the largest three numbers you can find.
+// 97, 26, 43
+// Can you solve for this example alone?
+// Hint, you had to find unique triplets in the last problem.
+
+
+// Think of this as an exercise
