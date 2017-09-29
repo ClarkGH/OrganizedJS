5511d6f0d8c1c9ad288b6292aa060f7f4a482bdc
clarkgh <demolisho@hotmail.com>
2017-09-29 09:37:20 -0700

    promise lesson 2

---
+++ JS/Intermediate/Promises/lesson2.js
@@ -0,0 +1,31 @@
+// Using Promises
+
+/*
+  In Javascript, a promise is an object that will return an eventual failure or completion of an async operations
+  The old callback logic:
+*/
+
+
+// Example 1: callback method
+function failureCallback( error ) {
+  throw new Error( 'Error, failure at: ' + error );
+}
+
+function successCallback( result ) {
+  console.warn( 'Success, running: ' + result );
+}
+
+someAsyncFunction(successCallback, failureCallback) //this function async returns an error or a result into the fed callbacks
+
+// Example2: Promise Method
+
+someAsyncFunction()
+  .then(successCallback, failureCallback); //.then() will wait until the promise is resolved and will run the code inside its block afterwards.
+
+// Nice things about promises vs callbacks
+
+/* 
+  Callbacks will never be called before the event loop finishes running (no race conditions)
+  Callbacks with .then() will run regardless of success or failure
+  You can chain many .then() or other promise object methods.
+*/
