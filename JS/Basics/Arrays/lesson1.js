6ac03ea920d233a6cc3671ed4b4efe83e553713a
clarkgh <demolisho@hotmail.com>
2017-10-03 19:16:48 -0700

    Resetting Array portion

--- JS/Basics/Arrays/lesson1.js
+++ JS/Basics/Arrays/lesson1.js
@@ -5,8 +5,23 @@ Arrays are objects that have their keys automatically assigned by index number
 The index starts from 0 and increments upwards
 */
 
-// Example 1: Array
+// Example 1: Array Creation
 var arr = [], //array literal
-  arr2 = new Array(3);
-console.log(arr); //will log the empty array, note that since it's an object it has some builtin methods
+  arr2 = new Array(3); // object constructor, avoid using this.
+console.log(arr); //will log the empty array, note that since it's an object it has some built-in methods
 console.log(arr2);
+
+// Example 2: Array Positions
+var nameContainer = ['Bill', 'Barbie', 'Bobby'];
+console.log(nameContainer[0]) //logs the [string], Bill
+console.log(nameContainer[1]) //logs the [string], Barbie
+
+// Example 3: Array Iteration
+// Review loops if this confuses you
+var nameContainer = ['Bill', 'Barbie', 'Bobby'];
+
+for ( var index = 0; index < nameContainer.length; i++ ) {
+  console.log( nameContainer[i] ); // What do you expect to see?
+}
+
+// Try creating your own array and play with it
