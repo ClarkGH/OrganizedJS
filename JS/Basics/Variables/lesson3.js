29d7dbe37ff03999169b1a412487b7d649a7c12e
clarkgh <demolisho@hotmail.com>
2017-10-11 18:05:14 -0700

    multi vars

---
+++ JS/Basics/Variables/lesson3.js
@@ -0,0 +1,14 @@
+// Variables: Declaring muliple variables
+
+// As a reference, it's best to use commas and declare variables at the top of scope
+
+// Example 1: Good/Clean way to declare variables
+var water = 'essential',
+  oxygen = water,
+  waffles = 'nom nom nom';
+
+// Too many vars clutter things up
+// Example 2: Not so good, more typing, way
+var water = 'essential';
+var oxygen = water;
+var waffles = 'nom nom nom';
