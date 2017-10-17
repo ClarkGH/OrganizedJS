94b0bb18ad3cd6528af9b977bcfcc465bfcbc45b
clarkgh <demolisho@hotmail.com>
2017-10-17 13:06:25 -0700

    Nodejs intro

---
+++ JS/Advanced/Nodejs/introduction.js
@@ -0,0 +1,18 @@
+// Node.js: an introduction
+
+/* 
+  - Node is a platform built on Chrome's V8 Javascript runtime
+  - It's used to build network applications that are both scalable and fast
+  - Build via a non-blocking and event-driven I/O model
+  - Lightweight and efficient, great with real-time applications that are potentially data intensive
+  - When people talk about node web applications, they generally mean express web apps
+*/
+
+// To Install
+
+/* Follow the instructions here: https://nodejs.org/en/
+  1. If installation was successful, type in terminal: 
+    a. $ node -version
+    b. $ npm -version
+  2. You should see a version returned
+*/
