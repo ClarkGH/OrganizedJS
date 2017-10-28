a6d2caddda9c0885be51e45c53de736df2052469
clarkgh <demolisho@hotmail.com>
2017-10-27 19:00:03 -0700

    backtick strings

---
+++ JS/Basics/Primitives/es2015/backtickStrings.js
@@ -0,0 +1,22 @@
+// ES2015: Backtick Strings
+
+/* 
+  As the name suggests, you can now use backticks to create strings
+  New features:
+    - Multi-line strings
+    - String interpolation
+*/
+
+// Example 1: Multi-line strings
+const threeLines = `
+barnabus
+boon
+borthus
+`
+console.log(fewLines);
+
+// Example 2: String Interpolation
+const name = "Daniel Boon",
+  greeting = `Howdy ${name}`;
+
+console.log(greeting);
