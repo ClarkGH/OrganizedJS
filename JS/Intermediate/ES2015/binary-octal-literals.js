06049dcd99d5b0f91ccbae0120d1247354dba988
clarkgh <demolisho@hotmail.com>
2017-10-28 19:20:12 -0700

    added old method for good measure

---
+++ JS/Intermediate/ES2015/binary-octal-literals.js
@@ -0,0 +1,12 @@
+// ES 2015 and Beyond: Binary and Octal literals
+
+/*
+  Javascript now has two useful built-in methods to translate binary into JS number values
+  I recommend reading up on binary/octal values to understand this section
+*/
+
+// Example 1: Binary and Octal values
+console.log(0b10000001) // 0b precedes the binary value
+console.log(0o767); // 0o precedes the octal value
+
+//This is better than having to use parseInt and change base ala parseInt('026', 8) or parseInt('001101', 2);
