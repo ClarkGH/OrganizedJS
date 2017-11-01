4127042bf10101d461cbaaa94f141a5da9caffe7
clarkgh <demolisho@hotmail.com>
2017-10-31 19:12:28 -0700

    functional programming desc

--- JS/Advanced/Functional Programming/lesson1.js
+++ JS/Advanced/Functional Programming/lesson1.js
@@ -1,1 +1,18 @@
-// 
+// Functional Programming
+
+/* 
+  Functional programming utilizes mathematical functions
+  It:
+    - Avoids shared state
+    - Avoids Mutability
+    - Uses PURE functions
+    - Avoids side-effects
+    - Is simple in composition
+    - Uses first class functions, higher order functions, and functions as arguments
+*/
+
+/* 
+  To understand functional programming, make sure you understand the following:
+    - Closures
+    - Callbacks
+*/
