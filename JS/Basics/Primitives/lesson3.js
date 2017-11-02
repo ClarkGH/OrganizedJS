ce6d9b460cca04acc2ce7667fe9adf69b8714877
clarkgh <demolisho@hotmail.com>
2017-11-01 18:09:10 -0700

    Quotes

---
+++ JS/Basics/Primitives/lesson3.js
@@ -0,0 +1,11 @@
+// Primitives: Strings
+
+/*
+  Strings are used to display text values
+*/
+
+// Examples
+console.log("Double quote string.");
+console.log('Single quote string.');
+console.log("Quotes within 'quotes'."); // You can do this with both single and double quotes
+console.log("Escaped \"Quotes\""); // Escaped characters are always preceded by a backtick
