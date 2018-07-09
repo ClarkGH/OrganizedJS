2b33faf809fb61ddfdfb6aa3a6d6550cab8cd1e1
clarkgh <demolisho@hotmail.com>
2018-07-08 17:08:32 -0700

    sorted

--- JS/Intermediate/Algorithms/sortObject.js
+++ JS/Intermediate/Algorithms/sortObject.js
@@ -1,9 +1,15 @@
 // How to sort objects
 // Object.keys(objectSorted).sort(sortingcallback())
 
-let list = { "bat": 44, "cow": 14, "bird": 23, "bear": 5 };
-let sortedKeys = Object.keys(list).sort(function (a, b) { 
-  return list[a] - list[b] 
+let list = { "bat": 44, "cow": 14, "bird": 23, "bear": 5 },
+  associativeArray = [];
+
+for (var animal in list) {
+  associativeArray.push([animal, list[animal]]);
+}
+
+associativeArray.sort(function (a, b) {
+  return a[1] - b[1];
 });
 
 console.log(sortedKeys);     
