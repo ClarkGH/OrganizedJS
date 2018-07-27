e8916eea29a19383152b9eec2928cf8f1a1d4e20
clarkgh <demolisho@hotmail.com>
2018-07-27 11:29:36 -0700

    sum an array w/ reduce

--- JS/Advanced/Functional Programming/map_filter_reduce/reduce/homework/answer.js
+++ JS/Advanced/Functional Programming/map_filter_reduce/reduce/homework/answer.js
@@ -1,3 +1,14 @@
+// log an array summed with .reduce()
+const arrayToSum = [1,2,3,4,5] //sums to 15
+
+console.log(
+  arrayToSum.reduce(
+    (memo, val) => {
+      return memo += val;
+    }
+  )
+);
+
 // With reduce, log a flattened 2d array
 
 const twoDimArray = [
