ff013e52daee56c78533fd6073e959ad55242178
clarkgh <demolisho@hotmail.com>
2018-07-27 11:34:16 -0700

    own reduce function

--- JS/Advanced/Functional Programming/map_filter_reduce/reduce/homework/answer.js
+++ JS/Advanced/Functional Programming/map_filter_reduce/reduce/homework/answer.js
@@ -22,3 +22,33 @@ console.log(
     return memo.concat(...val);
   })
 );
+
+// Write your own reduce function
+
+const arrayToReduce = [1, 2, 3, 4, 5];
+
+demoArray.manual_reduce = function (reducer, seedMemo) {
+  const isInitMemoIndexZero = arguments.length < 2;
+
+  let memo = isInitMemoIndexZero ? this[0] : seedMemo;
+
+  const initialIteratee = isInitMemoIndexZero ? 1 : 0;
+
+  for (var i = initialIteratee; i < this.length; i++) {
+    memo = reducer(memo, this[i]);
+  }
+
+  return memo;
+};
+
+//with less code... (not mine, but logic is good)
+
+arr.other_manual_reduce = function (reducer, seedMemo) {
+  let memo = seedMemo || this[0];
+  let i = seedMemo ? 0 : 1;
+
+  for (; i < this.length; i++) {
+    memo = reducer(memo, this[i]);
+  }
+  return memo;
+};
