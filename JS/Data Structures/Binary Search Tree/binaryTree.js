7ab014733a3d1bf7ea5f3d9c3b1bbefdf5a0793a
clarkgh <demolisho@hotmail.com>
2018-10-24 15:04:40 -0700

    insert

--- JS/Data Structures/Binary Search Tree/binaryTree.js
+++ JS/Data Structures/Binary Search Tree/binaryTree.js
@@ -3,26 +3,42 @@
 class BinarySearchTree {
   constructor(val) {
     this.val = val;
-    this.left = null;
-    this.right = null;
+    this.left = this.right = null;
   }
 
   insert(val) {
-    if (typeof val !== 'number') return 'undefined';
-    if (val <= this.value) {
-      if (!this.left === null) {
+    if (typeof val !== 'number') {
+      return
+    }
+    if (val <= this.val) {
+      if (!(this.left instanceof BinarySearchTree)) {
         this.left = new BinarySearchTree(val);
       } else {
-        this.left.insert(value);
+        this.left.insert(val);
       }
-    } else if (val > this.value) {
-      if (!this.right === null) {
+    } else if (val > this.val) {
+      if (!(this.right instanceof BinarySearchTree)) {
         this.right = new BinarySearchTree(val);
       } else {
-        this.right.insert(value);
+        this.right.insert(val);
       }
     }
   }
+
 } 
 
+
 let myTree = new BinarySearchTree(50);
+
+myTree.insert(10);
+myTree.insert(15);
+myTree.insert(20);
+myTree.insert(66);
+myTree.insert(99);
+myTree.insert(1);
+myTree.insert(100);
+myTree.insert(76);
+myTree.insert(22);
+myTree.insert(33);
+myTree.insert(56);
+myTree.insert(4);
