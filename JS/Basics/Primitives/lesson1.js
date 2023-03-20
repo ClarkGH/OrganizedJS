80f8da801872b6e917e9d131ad420cd6afa0337f
clarkgh <clarkhinchcliff@gmail.com>
2023-03-20 11:25:58 -0400

    It's alive: update lesson 1 and primitives lesson 1

--- JS/Basics/Primitives/lesson1.js
+++ JS/Basics/Primitives/lesson1.js
@@ -1,11 +1,17 @@
 // Primitives
 
 /*
-Primitives are basic values in Javascript
-They are NOT objects
+ * Primitives are basic values in Javascript.
+ * They are NOT objects
+ * Javascript currently has 7 primitives.
+ * 1. numbers
+ * 2. strings
+ * 3. booleans
+ * 4. symbols
+ * 5. undefined
+ * 6. null
+ * 7. bigInt
 */
-
-// Javascript currently has 6 primitives numbers, strings, booleans, symbols, undefined, and null
 
 1; // number
 "hi"; //string
@@ -13,3 +19,4 @@ true; // false
 Symbol('foo'); //symbol
 undefined; //undefined
 null; //null
+const x = BigInt(Number.MAX_SAFE_INTEGER); // 9007199254740991n
--- JS/Basics/lesson1.js
+++ JS/Basics/lesson1.js
@@ -1,30 +1,34 @@
-/* What Forms Javascript
-
-Javascript has 7 different data types
-  1. Numbers
-  2. Strings
-  3. Booleans
-  4. Symbol
-  5. Undefined
-  6. Null
-  7. Objects
-
-Each data type that is NOT AN OBJECT in JS, is a primitive value or primitive data type.
-
-Everything else is an object 
+/*
+ * What is this?
+ * Bunch of notes I wrote to help organize all of the mixed-up JS knowledge everywhere.
+ * 
+ * How can I use this?
+ * Review the primitives section first if you don't know much about javascript.
 */
+
+/* What Forms Javascript?
+ * Javascript has 8 different data types
+ * 1. Numbers
+ * 2. Strings
+ * 3. Booleans
+ * 4. Symbol
+ * 5. Undefined
+ * 6. Null
+ * 7. Objects
+ * 8. BigInt
+ *
+ * Everything that isn't on this list is considered an object.
+ */
 
 //Examples
 
 1; // number
-"hi"; //string
+"hi"; // string
 true; // false
-Symbol('foo'); //symbol
-undefined; //undefined
-null; //null
-function () {} //object
-[1, 2, 3] //object 'holding' primitive values
-//etc.
-
-// Review the primitives section first if you don't know where to start
-// After primitives move onto objects
+Symbol('foo'); // symbol
+undefined; // undefined
+null; // null
+{} // object
+function emptyFunction () {} // object
+[1, 2, 3] // object
+const gary = new Map(); // object
