d1ba2e9d39bd1fc55c61489eec08236e4a0f1e51
clarkgh <clarkhinchcliff@gmail.com>
2023-03-21 00:40:29 -0400

    Add spread/rest

--- JS/Basics/Functions/lesson2.js
+++ JS/Basics/Functions/lesson2.js
@@ -38,4 +38,3 @@ const add10 = makeAdder(10);
 
 console.log(add5(2)); // 7
 console.log(add10(2)); // 12
-
---
+++ JS/Intermediate/ES2015/spreadRest.js
@@ -0,0 +1,37 @@
+// Spread and Rest operators in JavaScript or '...'
+
+/**
+ * The Rest Params Operator '...' ...
+ * It's used specifically with function params.
+ * It Infers all arguments up to that argument's index into an array.
+ **/ 
+
+// Practical use-case
+const sum = (...nums) => {
+  return nums.reduce((a,b) => a + b);
+};
+
+sum(1, 2, 3, 4); // 10
+
+const sayHappyBirthdayGreeting = (age, name, ...words) => {
+  const birthdayMessage = words.join(' ');
+
+  return `Happy birthday ${name}, you are now of ${age} years. ${birthdayMessage}.`;
+};
+
+sayHappyBirthdayGreeting(98, 'Gary', 'You', 'are', 'old');
+
+/**
+ * The Spread Properties Operator '...'
+ * Used to explode object properties.
+ * Also used to explode array values.
+ * You can only call this inside function calls or object/array definitions
+*/
+
+const nums = [1, 2, 3];
+const newNums = [...nums, 4, 5, 6]; // [1, 2, 3, 4, 5, 6]
+const dog = { say: 'woof' };
+const hound = { ...dog, action: 'hunt' }; // { say: 'woof', action: 'hunt' }
+const words = ['It', 'is', 'you', 'birthday.', 'Consume', 'your', 'nutrition', 'paste'];
+
+sayHappyBirthdayGreeting(32, 'Logan', ...words);
